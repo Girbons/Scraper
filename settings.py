93d8ff1e5f6ac39fa391b62354662dc316d14eec
ALessandro <alessandrodea22@gmail.com>
2014-07-22 16:30:18 +0200

    first commit

---
+++ settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for circolari project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'circolari'
+
+SPIDER_MODULES = ['circolari.spiders']
+NEWSPIDER_MODULE = 'circolari.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'circolari (+http://www.yourdomain.com)'
